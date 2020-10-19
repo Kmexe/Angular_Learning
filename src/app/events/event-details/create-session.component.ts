bc2eb6823a822206718e417bcfe11f10cce61d7a
Jhonathan E <j_aristi@hotmail.com>
2020-10-19 10:29:10 -0500

    Adicionada comunicacion hijo-padre

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ISession, restrictedWords } from "../shared/index";
 
 @Component({
-    templateUrl: './create-session.component.html', styles: [`
+    selector: 'create-session',
+    templateUrl: './create-session.component.html',
+    styles: [`
     em { float:right; color: #e05c65; padding-left: 10px;}
     .error input, .error select, .error textarea {background-color:#e3c3c5;}
     .error ::-webkit-input-placeholder { color: #999:}
@@ -14,6 +16,8 @@ import { ISession, restrictedWords } from "../shared/index";
 })
 
 export class CreateSessionComponent implements OnInit {
+    @Output() saveNewSession = new EventEmitter()
+    @Output() cancelAddSession = new EventEmitter()
     newSessionForm: FormGroup
     name: FormControl
     presenter: FormControl
@@ -48,7 +52,9 @@ export class CreateSessionComponent implements OnInit {
             abstract: formValues.abstract,
             voters: []
         }
-        console.log(session)
+        this.saveNewSession.emit(session)
     }
-
+    cancel() {
+        this.cancelAddSession.emit()
+    }
 }
