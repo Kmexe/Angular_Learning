3cf9d68bc66da83dcde8452c42b1c77412fb3b4e
Jhonathan E <j_aristi@hotmail.com>
2020-10-19 13:53:43 -0500

    Adicionado Filtering y Sorting

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import { ISession } from '../shared/index'
 
 @Component({
@@ -6,7 +6,25 @@ import { ISession } from '../shared/index'
     templateUrl: './session-list.component.html'
 })
 
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
     @Input() sessions: ISession[]
+    @Input() filterBy: string
+    vissibleSessions: ISession[] = []
+
+    ngOnChanges() {
+        if (this.sessions) {
+            this.filterSessions(this.filterBy)
+        }
+    }
+
+    filterSessions(filter) {
+        if (filter === 'all') {
+            this.vissibleSessions = this.sessions.slice(0)
+        } else {
+            this.vissibleSessions = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter
+            })
+        }
+    }
 
 }
